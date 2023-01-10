57b7c02e2a2c287946668aff331005dd7522cd63
D-Ghoost <nicolas.torreslop@gmail.com>
2023-01-09 21:15:26 -0500

    clase 21, lista de productos

---
+++ main.js
@@ -0,0 +1,137 @@
+const menuEmail = document.querySelector(".navbar-email")
+const desktopMenu = document.querySelector(".desktop-menu")
+const burgerMenu = document.querySelector(".menu")
+const menuCarrito = document.querySelector(".navbar-shopping-cart")
+const mobileMenu = document.querySelector(".mobile-menu")
+// Previsualizacion del carrito
+const aside = document.querySelector(".product-detail")
+const cardsContainer = document.querySelector(".cards-container")
+
+// cuando haga click
+menuEmail.addEventListener('click', toggleDesktopMenu)
+burgerMenu.addEventListener('click', toggleMobileMenu)
+menuCarrito.addEventListener('click', toggleCarrito)
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+    
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+    // classList.toggle -> pone o quita la clase inactive
+    desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    // classList.toggle -> pone o quita la clase inactive
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarrito() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDeskMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed || !isDeskMenuClosed) {
+        mobileMenu.classList.add('inactive')
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
+}
+
+const productList = []
+
+productList.push({
+    name:'Bike',
+    price: 120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name:'Manga',
+    price: 50,
+    image:'https://aweita.cronosmedia.glr.pe/original/2021/07/14/60d38e30c5cc731b78371fbb.jpg'
+})
+
+productList.push({
+    name:'Audifonos',
+    price: 80,
+    image:'https://revistadc.com/wp-content/uploads/2020/09/VSG-mundo-gamer-audifonos-Singularity.jpg'
+})
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+        <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div>
+*/
+
+function renderProducts(arr) {
+    
+for (product of arr) {
+    const productCard = document.createElement('div')
+    productCard.classList.add('product-card')
+
+    const productImg = document.createElement('img')
+    // la primera variable es el atributo a cambiar, la segunda es el valor que tendra el atributo
+    productImg.setAttribute('src',product.image)
+
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-info')
+
+    const productInfoDiv = document.createElement('div')
+    const productPrice = document.createElement('p')
+    productPrice.innerText = '$'+product.price 
+    const productName = document.createElement('p')
+    productName.innerText = product.name
+
+    productInfoDiv.append(productPrice,productName)
+
+    const productInfoFigure = document.createElement('figure')
+    const productImgCart = document.createElement('img')
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg')
+
+    productInfoFigure.append(productImgCart)
+    productInfo.append(productInfoDiv,productInfoFigure)
+
+    productCard.append(productImg,productInfo)
+
+    cardsContainer.appendChild(productCard)
+
+}
+}
+
+renderProducts(productList)
+
+// *** Notas ***//
+
+// for con of( de ) 
+// es el recorrido de la lista(productList) y cada elemento se llamara product
+// asi que para saber el nombre del producto utilizamos product.name
+
+/* for (product of productList) {
+    console.log(product.name)
+} */
+
+// for con in( dentro )
+// es el recorrido de la lista(productList) y cada elemento se llamara product
+// a diferencia de for con of este nos trae el indice del elemento.
+
+/* for (product in productList) {
+    console.log(product)
+} */
